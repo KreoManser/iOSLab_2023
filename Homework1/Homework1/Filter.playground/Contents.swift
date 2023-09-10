87c37ff95f4d4a725144e4cdd8e22e32f58f80c2
kerikgg <kirill.abramov.24@mail.ru>
2023-09-11 01:14:01 +0300

    Выполнил ДЗ

---
+++ Homework1/Homework1/Filter.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//Доп Задания
+//1
+func longWordsFilter(words: [String]) -> [String] {
+    let filteredWords = words.filter { word in
+        return word.count > 5
+    }
+    return filteredWords
+}
+
+let words = ["aaaa", "bb", "nnnnmm", ""]
+print(longWordsFilter(words: words))
+
+//2
+func EvenNumbersFilter(numbers: [Int]) -> [Int] {
+    return numbers.filter {$0 % 2 == 0}
+}
+
+let numbers = [1,2,4,5,9,8,6,11]
+print(EvenNumbersFilter(numbers: numbers))
