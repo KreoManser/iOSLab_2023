38efef4aedf121b09f2b3be8762603662c05c8c4
Doosuur14 <127987952+Doosuur14@users.noreply.github.com>
2023-09-10 22:50:40 +0300

    Corrected Version

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+import UIKit
+
+let arrayOfWords = ["Mummy" , "Hello" , "Daily" , "Morining" , "Java" , "Filter" , "Numbers"]
+
+//function to check words with length five
+func fiveWordLength(_ input: [String]){
+    let lengthFive = arrayOfWords.filter{item in
+        return item.count == 5
+    }
+}
+fiveWordLength(arrayOfWords)
+
+
+
+var arrayOfNUmbers = [3 , 4 , 5 , 6 , 7 , 90 , 2 , 1 , 3, 4 , 5 , 6 , 7 , 8 , 0]
+
+//function to check if numbers are even
+func evenNumberCheck(_ input: [Int]){
+    let evenNumbers = arrayOfNUmbers.filter{ $0 % 2 == 0 }
+}
+evenNumberCheck(arrayOfNUmbers)
+
+
