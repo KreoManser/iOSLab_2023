003e8bb9f1cece5cb06079f8d882aa3fe6dbd24a
1k_mile <kamafeed1212@gmail.com>
2023-09-15 16:27:19 +0300

    сверстал первый экран и сделал доп задания с first

---
+++ 102/Homework_2/Homework_2/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Homework_2
+//
+//  Created by Камил Хайрутдинов on 13.09.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
