c2edbfc4f877b9befa4f6548a285ea5a539df555
Treisy-dev <vip.protop2004@gmail.com>
2023-09-17 22:38:22 +0300

    update playground

--- 102/ExtraTasks.playground/Contents.swift
+++ 102/ExtraTasks.playground/Contents.swift
@@ -4,13 +4,13 @@ import UIKit
 let numbers = [1, 3, 5, 6, 7, 8, 9, 10]
 
 
-if let firstEven = findFirstEvenNumber(in: numbers) {
+if let firstEven = getFirstEvenNumber(in: numbers) {
     print("Первое четное число в массиве: \(firstEven)")
 } else {
     print("Четных чисел в массиве нет.")
 }
 
-func findFirstEvenNumber(in numbers: [Int]) -> Int? {
+func getFirstEvenNumber(in numbers: [Int]) -> Int? {
     return numbers.first(where: { $0 % 2 == 0 })
 }
 
@@ -21,12 +21,12 @@ let words = ["apple", "banana", "cherry", "date", "fig"]
 let targetLetter: Character = "c"
 
 
-if let firstString = findFirstStringStartingWith(targetLetter, in: words) {
+if let firstString = getFirstStringStartedWith(targetLetter, in: words) {
     print("Первая строка, начинающаяся с буквы '\(targetLetter)': \(firstString)")
 } else {
     print("Нет строк, начинающихся с буквы '\(targetLetter)'.")
 }
 
-func findFirstStringStartingWith(_ letter: Character, in strings: [String]) -> String? {
+func getFirstStringStartedWith(_ letter: Character, in strings: [String]) -> String? {
     return strings.first(where: { $0.first == letter })
 }
