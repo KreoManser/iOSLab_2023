9fd07193bcc0614a4ce891061df0c506ac33d3b5
Doosuur14 <127987952+Doosuur14@users.noreply.github.com>
2023-09-18 00:45:27 +0300

    Add files via upload
    SecondHomeWork

---
+++ 102/Homework2/Homework2/MySecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MySecondViewController.swift
+//  Homework2
+//
+//  Created by Faki Doosuur Doris on 17.09.2023.
+//
+
+import UIKit
+
+class MySecondViewController: UIViewController {
+    @IBOutlet weak var mySecondImageview: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewDidAppear()
+    }
+    
+    func viewDidAppear() {
+        mySecondImageview.layer.cornerRadius = mySecondImageview.frame.height/2
+        mySecondImageview.layer.masksToBounds = true
+        self.view.backgroundColor = .systemIndigo
+    }
+    
+    @IBAction func exitButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "unWindToFirst", sender: self)
+    }
+}
