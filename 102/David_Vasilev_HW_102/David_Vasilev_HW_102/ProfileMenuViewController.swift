1d544e27ef40297f16ae193f5686f1d5d17e3816
David-11209 <limonchik087@gmail.com>
2023-09-18 02:28:22 +0300

    HW_102_David_Vasilev

---
+++ 102/David_Vasilev_HW_102/David_Vasilev_HW_102/ProfileMenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileMenuViewController.swift
+//  David_Vasilev_HW_102
+//
+//  Created by Давид Васильев on 18.09.2023.
+//
+
+import UIKit
+
+class ProfileMenuViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+     
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 102/David_Vasilev_HW_102/David_Vasilev_HW_102/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  David_Vasilev_HW_102
+//
+//  Created by Давид Васильев on 18.09.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
