82e2bedf689659c2fca18d2ff76551bd585a4888
timergOoOt <timur18092004@gmail.com>
2023-09-18 08:40:44 +0300

    feat: add basket page and extra task in playground

--- 102/HW_2/HW_2/ShoppingBasketViewController.swift
+++ 102/HW_2/HW_2/ShoppingBasketViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class ShoppingBasketViewController: UIViewController {
-
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.setHidesBackButton(true, animated: false)
+        
+        totalLabel.layer.cornerRadius = 5.0
+        totalLabel.clipsToBounds = true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
