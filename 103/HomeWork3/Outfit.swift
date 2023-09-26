d7e29565783843496fad4a9fa7a4292e5e65cbf8
Doosuur14 <127987952+Doosuur14@users.noreply.github.com>
2023-09-27 00:42:47 +0300

    Add files via upload

---
+++ 103/HomeWork3/Outfit.swift
@@ -0,0 +1,16 @@
+//
+//  Outfit.swift
+//  HomeWork3
+//
+//  Created by Faki Doosuur Doris on 26.09.2023.
+//
+
+import Foundation
+import UIKit
+
+struct Outfit {
+    let name: String
+    let price: String
+    let size: String
+    let avatarImage: UIImage?
+}
