53f94d78bcd0c2f3b2117e7e0dbf4421f6324ede
1k_mile <kamafeed1212@gmail.com>
2023-09-29 19:02:52 +0300

    сверстал начальный экран

--- 104/HomeWork_4/HomeWork_4/ToDoTasksViewController.swift
+++ 104/HomeWork_4/HomeWork_4/ToDoTasksViewController.swift
@@ -8,24 +8,31 @@
 import UIKit
 
 // MARK: - TasksViewController
-class TasksViewController: UIViewController {
+class ToDoTasksViewController: UIViewController {
     // MARK: - UI elements
     private lazy var tasksTableView: UITableView = {
         let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
+    }()
+    
+    private lazy var customBarButtonItem: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "FilterButtonImg"), for: .normal)
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        self.title = "Заметки"
         addSubViews(tasksTableView)
         configureUI()
+        setupNavigationBar()
     }
 }
 
 // MARK: - extencion TasksViewController
-extension TasksViewController {
+extension ToDoTasksViewController {
     private func addSubViews(_ subviews: UIView...) {
         subviews.forEach { view.addSubview($0) }
     }
@@ -37,5 +44,20 @@ extension TasksViewController {
             tasksTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
             tasksTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
         ])
+    }
+    
+    private func setupNavigationBar() {
+        self.title = "To Do"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .add, primaryAction: nil)
+        
+        let cusmotButton = UIBarButtonItem(customView: customBarButtonItem)
+        cusmotButton.customView?.widthAnchor.constraint(equalToConstant: 17).isActive = true
+        cusmotButton.customView?.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        
+        navigationItem.rightBarButtonItems =
+        [
+            UIBarButtonItem(systemItem: .edit, primaryAction: nil),
+            cusmotButton
+        ]
     }
 }
