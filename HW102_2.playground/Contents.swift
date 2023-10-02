896315018385ddef8bad8201c7efaa5a84048489
ZiganshinAzat <azv7_78@mail.ru>
2023-10-02 18:22:01 +0300

    Удалил лишние проекты

--- HW102_2.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-func findFirstEvenNumber(numbers: [Int]) -> Int? {
-    return numbers.first { $0 % 2 == 0 }
-}
-
-func findFirstStringStartingWith(letter: Character, _ strings: [String]) -> String? {
-    return strings.first(where: { $0.first == letter })
-}
