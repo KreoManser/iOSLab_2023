0a260501a3399657636d80b58f915c7fd49f9c30
T3mag(Артур) <113981369+T3mag@users.noreply.github.com>
2023-10-02 09:49:02 +0300

    HW_104_notFinishVersion загвостка в том, что я не могу написать полноценный проект, т.к  после обновления XCode я не могу установить iOS

---
+++ 104/HW4_ToDoList/HW4_ToDoList/ViewControllers/TaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTableViewCell.swift
+//  HW4_ToDoList
+//
+//  Created by Артур Миннушин on 02.10.2023.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
