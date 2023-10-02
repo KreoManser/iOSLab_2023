1609c5af3df08cc5538967f1285686cfd99a4164
T3mag(Артур) <113981369+T3mag@users.noreply.github.com>
2023-10-02 09:50:39 +0300

    HW_104_notFinishVersion загвостка в том, что я не могу написать полноценный проект, т.к  после обновления XCode я не могу установить iOS. Можно будет дополнить код?

--- 104/HW4_ToDoList/HW4_ToDoList/ViewControllers/TaskTableViewCell.swift
+++ 104/HW4_ToDoList/HW4_ToDoList/ViewControllers/TaskTableViewCell.swift
@@ -7,17 +7,60 @@
 
 import UIKit
 
+protocol UserCellDelegate: AnyObject {
+    func didPressDetailDisclosure()
+}
+
 class TaskTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    weak var delegate: UserCellDelegate?
+    
+    private lazy var taskName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var descriptionTask: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(with task: Task) {
+        
+        taskName.text = task.nameTask
+        descriptionTask.text = task.descriptionTask
+    }
+    
+    private func setupLayout() {
+        let mainStackView = UIStackView(arrangedSubviews: [taskName, descriptionTask])
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fillEqually
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 10),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)])
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    
+}
 
-        // Configure the view for the selected state
+extension UITableViewCell {
+    static var reuseIdentifier: String {
+        return String(describing: self)
     }
-
 }
--- 104/HW4_ToDoList/HW4_ToDoList/ViewControllers/TaskViewController.swift
+++ 104/HW4_ToDoList/HW4_ToDoList/ViewControllers/TaskViewController.swift
@@ -7,13 +7,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TaskViewController: UIViewController, UITableViewDelegate, UserCellDelegate {
+    func didPressDetailDisclosure() {
+        <#code#>
+    }
+    
+    
+    var tasks: [Task] = []
+    var dataSource: UITableViewDiffableDataSource<tableSelection, Task>?
+    var dataBase: [Task] = [
+        Task(id: UUID(), nameTask: "Сделать ДЗ", descriptionTask: "Сделать четвертое задание из курса iOs"),
+        Task(id: UUID(), nameTask: "Пойти гулять", descriptionTask: "Сделать четвертое задание из курса iOs"),
+        Task(id: UUID(), nameTask: "Купить продукты", descriptionTask: "Сделать четвертое задание из курса iOs"),
+        Task(id: UUID(), nameTask: "Лечь спать", descriptionTask: "Сделать четвертое задание из курса iOs"),
+        Task(id: UUID(), nameTask: "Забрать посылку", descriptionTask: "Сделать четвертое задание из курса iOs")]
+    
+    enum tableSelection{
+        case main
+        case secondary
+    }
+    
+    lazy var tableView: UITableView = {
+       
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.separatorStyle = .none
+        table.register(TaskTableViewCell.self, forCellReuseIdentifier: TaskTableViewCell.reuseIdentifier)
+        table.estimatedRowHeight = 80
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0..<5 {
+            tasks.append(dataBase[i])
+        }
+        
+        view.backgroundColor = .lightGray
+        view.addSubview(tableView)
+        
+        
+        setuplayout()
+        setupNavigationBar()
+        setupDataSource()
+        
     }
-
-
+    
+    func setuplayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            ])
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Task"
+        
+    }
+    
+    func setupDataSource() {
+        dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, task in
+            let cell = tableView.dequeueReusableCell(withIdentifier: TaskTableViewCell.reuseIdentifier, for: indexPath) as! TaskTableViewCell
+            cell.configureCell(with: task)
+            cell.delegate = self
+            return cell
+        })
+        updateDataSource(with: tasks)
+    }
+    
+    func updateDataSource(with tasks: [Task]) {
+        var snapshot = NSDiffableDataSourceSnapshot<tableSelection, Task>()
+        snapshot.appendSections([.main, .secondary])
+        snapshot.appendItems(tasks)
+        dataSource?.apply(snapshot, animatingDifferences: false)
+        
+        
+    }
 }
 
