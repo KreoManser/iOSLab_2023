c2c10b2a9016301965f334c0ef6866695db7aa8d
1k_mile <kamafeed1212@gmail.com>
2023-10-03 23:54:08 +0300

    добавил проект

---
+++ 105/HomeWork_5/HomeWork_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_5
+//
+//  Created by Камил Хайрутдинов on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
