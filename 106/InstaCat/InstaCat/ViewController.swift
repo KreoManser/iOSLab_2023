69da87b7dd20e879f3f4dc8a5967b48abc263379
1k_mile <kamafeed1212@gmail.com>
2023-10-28 16:39:36 +0300

    пересоздал прод

---
+++ 106/InstaCat/InstaCat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCat
+//
+//  Created by Камил Хайрутдинов on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
+
