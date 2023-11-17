130b06825906a5e706b4b714dfede8df41565267
1k_mile <kamafeed1212@gmail.com>
2023-11-17 09:55:36 +0300

    add prod with lint

---
+++ 108/MyGame/MyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGame
+//
+//  Created by Камил Хайрутдинов on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
