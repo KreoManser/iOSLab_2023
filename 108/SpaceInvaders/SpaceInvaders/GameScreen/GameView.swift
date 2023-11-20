f4c22060384c61adc7c6ba66e84afbd867cdaf8b
Ruslan Kozlov <ruslankozlov@MacBook-Pro-Ruslan.local>
2023-11-20 13:31:45 +0500

    108hw

---
+++ 108/SpaceInvaders/SpaceInvaders/GameScreen/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  SpaceInvaders
+//
+//  Created by Ruslan Kozlov on 15.11.2023.
+//
+
+import UIKit
+
+class GameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 108/SpaceInvaders/SpaceInvaders/GameScreen/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpaceInvaders
+//
+//  Created by Ruslan Kozlov on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
